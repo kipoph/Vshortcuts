57a784189860be990d7f12e27ef54ffb4126be13
kipoph <kipoph1@gmail.com>
2020-09-08 17:59:22 +0300

    finish first draft

--- content.js
+++ content.js
@@ -1,20 +1,16 @@
 window.addEventListener("keydown",checkKey,false) //listen for keydown
 function checkKey(e){
-      if (e.keyCode==221){ //if key is right bracket increase playbackRate by 25 percent
-        console.log('lets do this')
-        document.getElementsByTagName('video')[0].playbackRate +=0.25
+  if (e.keyCode==221){ //if key is right bracket increase playbackRate by 25 percent
+    if(e.shiftKey){
+      document.getElementsByTagName('video')[0].playbackRate +=0.5
+    } else{
+      document.getElementsByTagName('video')[0].playbackRate +=0.25
     }
+  }else if(e.keyCode==219){
+    if(e.shiftKey){
+      document.getElementsByTagName('video')[0].playbackRate -=0.5
+    }else{
+      document.getElementsByTagName('video')[0].playbackRate -=0.25
     }
- /* switch(event.code){
-        case 'BracketLeft':
-        document.getElementsByTagName('video')[0].playbackRate -=0.25
-        console.log('speedDown')
-        break;
-        case 'BracketRight':
-        document.getElementsByTagName('video')[0].playbackRate +=0.25
-        alert('speedUp')
-        break;
-        default:
-        break;
-    
-    return*/
+  }
+}
